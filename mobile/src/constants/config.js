910e878f65ca22a853cfc5201297ac04c03e43f2
GLAIRA YVONNE DALISAY <glaira.yvonne.dalisay@estudiantat.upc.edu>
2025-12-16 07:22:37 +0100

    chore: untrack config.js

--- mobile/src/constants/config.js
+++
@@ -1,6 +0,0 @@
-// Encuentra tu IP local:
-// - Linux/Mac: hostname -I | awk '{print $1}'
-// - Windows: ipconfig
-// Busca algo como 192.168.X.X
-
-export const API_BASE_URL = 'http://172.20.10.2:3000';
